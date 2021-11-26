6e6a8d5fc75e26f47ae223d2a9e727b3f0945a3b
luka598 <luka598@tuta.io>
2021-11-26 05:54:07 +0100

    For some reason git didn't delete old files.

--- convert.py
+++
@@ -1,5 +0,0 @@
-def plaintext(text):
-	plain_text = ""
-	for i in text:
-		plain_text += i.plain_text()
-	return plain_text
--- main.py
+++
@@ -1,9 +0,0 @@
-from parse import parse
-from convert import plaintext
-
-with open("test.yamd", "r") as f:
-	file = f.read().replace("\t", "")
-
-tokenized = parse(file)
-
-print(plaintext(tokenized))
